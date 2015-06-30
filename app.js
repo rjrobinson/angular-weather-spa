9a929562e3ec9500ef945aad1f4b91246753d65d
RJ Robinson <robertj.robinson@gmail.com>
2015-06-29 22:24:06 -0400

    connected to external weather API

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ weatherApp.config(['$routeProvider', function($routeProvider) {
             templateUrl: 'pages/home.html',
             controller: 'homeController'
         })
-        .when('/forcast', {
+        .when('/forecast', {
             templateUrl: 'pages/forecast.html',
             controller: 'forecastController'
         })
@@ -41,16 +41,43 @@ weatherApp.controller('homeController', ['$scope', 'cityService',
 
         $scope.$watch('city', function() {
             cityService.city = $scope.city
-        })
+        });
+
+
 
     }
 ])
 
 // -- CONTROLLER :: FORECAST
-weatherApp.controller('forecastController', ['$scope', 'cityService',
-    function($scope, cityService) {
+weatherApp.controller('forecastController', ['$scope', '$resource', 'cityService',
+    function($scope, $resource, cityService) {
 
         $scope.city = cityService.city;
+
+        $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily", {
+            callback: "JSON_CALLBACK"
+        }, {
+            get: {
+                method: "JSONP"
+            }
+        });
+
+        $scope.weatherResult = $scope.weatherAPI.get({
+            q: $scope.city,
+            cnt: $scope.days
+        });
+        $scope.convertToFahrenheit = function(degK) {
+
+            return Math.round((1.8 * (degK - 273)) + 32);
+
+        }
+
+        $scope.convertToDate = function(dt) {
+
+            return new Date(dt * 1000);
+
+        };
+
     }
 ])
 
